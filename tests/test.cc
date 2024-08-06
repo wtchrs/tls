cb20c927c0a3a84c43b96a33f6f25801e3bbe02e
wtchrs <wtchr_@hotmail.com>
2024-08-06 19:35:04 +0900

    Add a class for Diffie-Hellman key exchange protocol

--- tests/test.cc
+++ tests/test.cc
@@ -3,6 +3,7 @@
 //
 
 #include <catch2/catch_test_macros.hpp>
+#include "diffie_hellman.h"
 #include "mpz.h"
 
 TEST_CASE("mpz") {
@@ -11,4 +12,10 @@ TEST_CASE("mpz") {
     mpz2bnd(a, arr, arr + 8);
     mpz_class b = bnd2mpz(arr, arr + 8);
     REQUIRE(a == b);
+}
+
+TEST_CASE("Diffie Hellman") {
+    diffie_hellman alice, bob;
+    REQUIRE(alice.set_peer_public_key(bob.y) == bob.set_peer_public_key(alice.y));
+    REQUIRE(alice.K == bob.K);
 }
