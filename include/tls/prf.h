aa420f943a803686fe64d6d4ea0f2f93289a5442
wtchrs <wtchr_@hotmail.com>
2025-01-14 21:44:15 +0900

    Add pseudo-random function

---
+++ include/tls/prf.h
@@ -0,0 +1,103 @@
+#include <algorithm>
+#include "tls/hmac.h"
+
+
+/**
+ * @brief Pseudo-Random Function (PRF) implementation for TLS protocol
+ *
+ * This class implements the PRF function as defined in TLS specifications.
+ * It generates a pseudo-random byte sequence using HMAC with a specified
+ * hash function, secret, label, and seed.
+ *
+ * @tparam Hash The hash function to be used (must satisfy HashFunction concept)
+ */
+template<HashFunction Hash>
+class prf {
+private:
+    hmac<Hash> hmac_; ///< HMAC object
+    std::vector<unsigned char> label_, seed_; ///< Label and seed data
+
+public:
+    /**
+     * @brief Sets the secret key for the PRF
+     *
+     * @tparam IT Iterator type for the input range
+     * @param begin Iterator to the start of the secret key
+     * @param end Iterator to the end of the secret key
+     */
+    template<typename IT>
+    void secret(const IT begin, const IT end);
+
+    /**
+     * @brief Sets the label string for the PRF
+     *
+     * The label is a null-terminated string that is used as part of
+     * the input to the PRF calculation.
+     *
+     * @param p Pointer to null-terminated label string
+     */
+    void label(const char *p);
+
+    /**
+     * @brief Sets the seed data for the PRF
+     *
+     * @tparam IT Iterator type for the input range
+     * @param begin Iterator to the start of the seed data
+     * @param end Iterator to the end of the seed data
+     */
+    template<typename IT>
+    void seed(const IT begin, const IT end);
+
+    /**
+     * @brief Generates n pseudo-random bytes
+     *
+     * Implements the TLS PRF algorithm to generate the requested number
+     * of pseudo-random bytes using the configured secret, label, and seed.
+     *
+     * @param n Number of bytes to generate
+     * @return std::vector<unsigned char> Vector containing n pseudo-random bytes
+     */
+    std::vector<unsigned char> get_n_bytes(size_t n);
+};
+
+
+template<HashFunction Hash>
+template<typename IT>
+void prf<Hash>::secret(const IT begin, const IT end) {
+    hmac_.key(begin, end);
+}
+
+template<HashFunction Hash>
+void prf<Hash>::label(const char *p) {
+    label_.clear();
+    while (*p) {
+        label_.push_back(*p++);
+    }
+}
+
+template<HashFunction Hash>
+template<typename IT>
+void prf<Hash>::seed(const IT begin, const IT end) {
+    seed_.clear();
+    for (IT it = begin; it != end; ++it) {
+        seed_.push_back(*it);
+    }
+}
+
+template<HashFunction Hash>
+std::vector<unsigned char> prf<Hash>::get_n_bytes(size_t n) {
+    // seed = A(0) = label_ + seed_
+    auto seed = label_;
+    seed.insert(seed.end(), seed_.cbegin(), seed_.cend());
+    std::vector<unsigned char> r, v;
+    /* std::array<unsigned char, Hash::output_size> */ auto A = hmac_.hash(seed.cbegin(), seed.cend()); // A(1)
+    v.resize(A.size()); // A(i) will be copied into this new space.
+    v.insert(v.end(), seed.cbegin(), seed.cend());
+    for (; r.size() < n; /* Calculate A(i) */ A = hmac_.hash(A.cbegin(), A.cend())) {
+        std::copy(A.cbegin(), A.cend(), v.begin());
+        auto next_block = hmac_.hash(v.cbegin(), v.cend());
+        r.insert(r.end(), next_block.cbegin(), next_block.cend());
+    }
+    r.resize(n);
+    return r;
+}
---
+++ tests/prf.cpp
@@ -0,0 +1,27 @@
+#include "tls/prf.h"
+#include <catch2/catch_test_macros.hpp>
+#include <gmp.h>
+#include <gmpxx.h>
+#include <vector>
+#include "tls/mpz.h"
+#include "tls/sha/sha1.h"
+
+TEST_CASE("PRF test") {
+    prf<sha1> prf;
+    unsigned char seed[100], secret[100];
+    std::vector<std::vector<unsigned char>> vv;
+    mpz_class z1{"0x3a64b675191395ba19842ad7d14c2d798fe9e2dab6b9ebcdfab50ec68a862691effbff693bc68643a6463c71b322c9d7cb3"
+                 "e0b29c15dbee6d11d42667a014183"};
+    mpz_class z2{"0xc5048557a1a02314403003ee56326aaf33bc3c10fd7f00007280a784ca5500006b9ccfad52e06aedb01f4eab6c2caaa6"};
+    mpz_class expected{"0x3b6b817ecb6fd456d4989b24832ecdad44a8349bc0c7551d84fb2da638909846fbb1f984f4b35b6ff7103e687493b"
+                       "3e7b7296096fcb3ee8358082da129eaceb4766e1f20cdf25901"};
+    int sz1 = (mpz_sizeinbase(z1.get_mpz_t(), 16) + 1) / 2;
+    int sz2 = (mpz_sizeinbase(z2.get_mpz_t(), 16) + 1) / 2;
+    mpz2bnd(z1, seed, seed + sz1);
+    mpz2bnd(z2, secret, secret + sz2);
+    prf.label("master secret");
+    prf.seed(seed, seed + sz1);
+    prf.secret(secret, secret + sz2);
+    auto a = prf.get_n_bytes(72);
+    REQUIRE(expected == bnd2mpz(a.cbegin(), a.cend()));
+}
